5b7cd4181154ebdded571200d5ba8320aa42151c
blewis49 <jblewis49@me.com>
2018-04-25 21:03:53 -0400

    correction to spelling of Australia

--- aus_salary.R
+++ aus_salary.R
@@ -1,3 +1,7 @@
+## Tidy Tuesday week 4 
+## High Income Female Occupations in Australia
+## jblewis49
+## ------------------------------
 library(tidyverse)
 aus.sal <- read.csv("./tidytuesday-0.1.04/data/week4_australian_salary.csv")
 
@@ -18,16 +22,16 @@ plot1 <- left_join(top.10.female, all.male, by = "occupation") %>%
       geom_point(aes(x = fct_reorder(occupation, average_taxable_income.y / 1000), 
                      y = average_taxable_income.y / 1000), color = "light blue", size = 5) +
       coord_flip() + 
-      ggtitle("Top 10 Highest Income Occupations for Females in Austrailia [pink]", 
+      ggtitle("Top 10 Highest Income Occupations for Females in Australia [pink]", 
               subtitle = "Comparison of Male Counterpart Salaries [blue]") +
       labs(x = NULL, 
-           y = "Average Income (100K$)", 
+           y = "Average Income ($K)", 
            caption = "Source: data.gov.au   |   graphic by jblewis49") + 
       theme(plot.caption = element_text(size = 8, hjust = 0))
       
 ggsave("fem_high_salary_plot.png",  plot = last_plot(), width = 12, height = 6, dpi = 600)   
       
-# ---------- Top 10 Highest Income Occupations for Males in Austrailia --------
+# ---------- Top 10 Highest Income Occupations for Males in Australia --------
       
 top.10.male <- aus.sal %>% 
       filter(gender == "Male") %>%
